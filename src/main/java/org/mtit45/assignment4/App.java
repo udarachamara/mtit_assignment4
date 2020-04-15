cb6a6a09f839a7c79d84492c4a763e79b2dad17e
IT16113732 <it16113732@my.sliit.lk>
2020-04-15 09:01:38 +0530

    add swagger configuration

--- src/main/java/org/mtit45/assignment4/App.java
+++ src/main/java/org/mtit45/assignment4/App.java
@@ -2,11 +2,6 @@ package org.mtit45.assignment4;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import springfox.documentation.builders.PathSelectors;
-import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.spi.DocumentationType;
-import springfox.documentation.spring.web.plugins.Docket;
 
 @SpringBootApplication
 public class App {
@@ -15,11 +10,4 @@ public class App {
         SpringApplication.run(App.class, args);
     }
 
-//    @Bean
-//    public Docket api() {
-//        return new Docket(DocumentationType.SWAGGER_2).select()
-//                .apis(RequestHandlerSelectors.basePackage("org.mtit45.assignment4.controller"))
-//                .paths(PathSelectors.any())
-//                .build();
-//    }
 }
